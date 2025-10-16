dcc6439078922505bd51fe896590cbc5275d5e12
Lucas-blancaws <lblancportier@gmail.com>
2025-10-16 16:09:26 +0200

    Fix namespaces pour users et lieux dans l'API Flask

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Api
 
 from app.api.v1.users import api as users_ns
-
+from app.api.v1.places import api as lieux_ns
 def create_app():
     app = Flask(__name__)
     api = Api(
@@ -14,5 +14,6 @@ def create_app():
     )
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(lieux_ns, path='/api/v1/lieux')
 
     return app
