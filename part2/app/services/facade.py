0342416afa8431884cc803214d62198a605b7424
Lucas-blancaws <lblancportier@gmail.com>
2025-10-16 10:45:43 +0200

    implémentation de HBnBFacade pour centraliser la logique utilisateur

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,18 +1,37 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import Utilisateur
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-    # Méthode de remplacement pour créer un utilisateur
     def create_user(self, user_data):
-        # La logique sera mise en œuvre dans les tâches ultérieures.
-        pass
+        if self.get_user_by_email(user_data['email']):
+            return None  # email déjà utilisé
+        user = Utilisateur(**user_data)
+        self.user_repo.add(user)
+        return user.to_dict(include_password=False)
 
-    # Méthode de remplacement pour récupérer un lieu par son identifiant
-    def get_place(self, place_id):
-        # La logique sera mise en œuvre dans les tâches ultérieures.
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+    
+    def get_all_users(self):
+        users = self.user_repo.get_all()
+        user_dicts = []
+        for user in users:
+            user_dicts.append(user.to_dict(include_password=False))
+        return user_dicts
+    
+    def update_user(self, user_id, data):
+        """
+        Met à jour un utilisateur existant.
+        Retourne le dict mis à jour sans mot de passe.
+        """
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+        user.update(data)
+        return user.to_dict(include_password=False)
