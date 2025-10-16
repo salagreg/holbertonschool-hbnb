31c043163fc3e1980ff4b5847280c1045dbe0bcc
Lucas-blancaws <lblancportier@gmail.com>
2025-10-16 16:10:47 +0200

    Création de l'application Flask et enregistrement du namespace Lieux pour l'API

--- part2/run.py
+++ part2/run.py
@@ -1,6 +1,12 @@
-from app import create_app
+from flask import Flask
+from flask_restx import Api
+from app.api.v1.places import api as lieu_ns
 
-app = create_app()
+app = Flask(__name__)
+api = Api(app, title="HBnB API", version="1.0", description="API de gestion des lieux et utilisateurs")
 
-if __name__ == '__main__':
+# Enregistre le namespace
+api.add_namespace(lieu_ns, path="/lieux")
+
+if __name__ == "__main__":
     app.run(debug=True)
